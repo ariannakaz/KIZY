2f4da2adda695a5cc65b5fcd3c0c7109baf3368d
Efosa Ighodaro <efosaighodaroei@gmail.com>
2019-11-21 14:16:12 -0500

    Add files via upload

--- shell/shell.js
+++ shell/shell.js
@@ -3,11 +3,21 @@ $(".likert").click( function () {
     document.getElementById("q1").style.display = "none";
     
 })
+$("#question2").click( function () {
+    document.getElementById("question2").style.display = "none";
+    document.getElementById("next").style.display = "block";
+    
+})
 document.getElementById("type").addEventListener("click" , function (event) {
     
     
 })
-
+$("#ptab").click( function () {
+    document.getElementsByClassName("challenge").style.display = "block";
+})
+$("#mtab").click( function () {
+    document.getElementsByClassName("challenge").style.display = "block";
+})
 
 /*
 function url() {
