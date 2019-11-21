6c96f12dcae9ca7549949c2ab2d8c01cdf973d63
Efosa Ighodaro <efosaighodaroei@gmail.com>
2019-11-21 13:22:49 -0500

    Add files via upload

--- shell/shell.js
+++ shell/shell.js
@@ -3,3 +3,13 @@ $(".likert").click( function () {
     document.getElementById("q1").style.display = "none";
     
 })
+document.getElementById("type").addEventListener("click" , function (event) {
+    
+    
+})
+
+
+/*
+function url() {
+    window.location.href = 'Handler1.ashx?username' 
+}*/
