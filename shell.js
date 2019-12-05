e47dff15657812923adf5d9e7e77e5dce2b10536
Alexanderyang5 <36316645+Alexanderyang5@users.noreply.github.com>
2019-12-05 14:12:06 -0500

    Add files via upload

--- shell.js
+++ shell.js
@@ -1,8 +1,28 @@
+// radio value minutes
+let radioNum = 0;
+
 $(".likert").click( function () {
+    // alex edit
+    if(document.getElementById("5").checked  == true) {
+        radioNum = document.getElementById("5").value
+    }
+    if(document.getElementById("10").checked  == true) {
+        radioNum = document.getElementById("10").value
+    }
+    if(document.getElementById("15").checked  == true) {
+        radioNum = document.getElementById("15").value
+    }
+    if(document.getElementById("20").checked  == true) {
+        radioNum = document.getElementById("20").value
+    }
+    if(document.getElementById("30").checked  == true) {
+        radioNum = document.getElementById("30").value
+    }  
+
     document.getElementById("question2").style.display = "block";
     document.getElementById("q1").style.display = "none";
-    
 })
+
 $("#question2").click( function () {
     document.getElementById("question2").style.display = "none";
     document.getElementById("next").style.display = "block";
